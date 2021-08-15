96f81d0ed77d8ea0f33a76a562cb41a823ffc419
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-08-15 13:56:21 +0000

    [Update]

--- country.rb
+++ country.rb
@@ -1,8 +1,8 @@
 country = 'italy'
 
 case country
-when 'japan' then 'こんにちは'
-when 'us' then 'Hello'
-when 'italy' then 'ciao'
-else '???'
+ when 'japan' then 'こんにちは'
+ when 'us' then 'Hello'
+ when 'italy' then 'ciao'
+ else '???'
 end
---
+++ point.rb
@@ -0,0 +1,5 @@
+point = 7
+day = 1
+if day == 1
+  point *= 5
+end
